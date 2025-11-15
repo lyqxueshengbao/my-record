932b8ec09d8ff0288ef2e2445b661504278bff19
lyqFlaringFire <2513598550@qq.com>
2025-11-15 15:49:54 +0800

    online第一次修改

--- models/record_oi.py
+++ models/record_oi.py
@@ -5,7 +5,8 @@ from .layers.bottleneck_lstm import BottleneckLSTM
 from utils.models_utils import _make_divisible
 
 """
-Online Inference version of RECORD model with Hybrid Norm + ECA
+Online Inference 版本 - 使用 Hybrid Norm + ECA
+每次只处理单帧,保持LSTM状态
 """
 
 
@@ -13,62 +14,64 @@ class RecordOI(nn.Module):
     def __init__(self, config, in_channels=8, norm='layer', n_class=3):
         """
         RECurrent Online object detectOR (RECORD) model class for online inference
-        *** 修改版 (Hybrid Norm + ECA) ***
+        *** 修改版: Hybrid Norm + ECA ***
 
         @param config: configuration file of the model
         @param in_channels: number of input channels (default: 8)
-        @param norm: type of normalisation for recurrent parts (default: 'layer')
+        @param norm: type of normalisation for recurrent/decoder parts (default: 'layer')
         @param n_class: number of classes (default: 3)
         """
         super(RecordOI, self).__init__()
 
-        # Hybrid Norm: Stem 用 BN，Recurrent 用 LayerNorm/GN(1)
+        # Encoder: Hybrid Norm (BN for stem, GN for recurrent)
+        # 注意：参数顺序和名称必须匹配RecordEncoder的定义
         self.encoder = RecordEncoder(
-            config=config['encoder_config'],
             in_channels=in_channels,
-            norm_stem='bn',  # Stem 部分使用 BatchNorm
-            norm_recurrent=norm  # Recurrent 部分使用 LayerNorm
+            config=config['encoder_config'],
+            norm_stem='bn',  # Stem部分使用BatchNorm
+            norm_recurrent=norm  # Recurrent部分使用LayerNorm/GroupNorm
         )
 
-        # Decoder 使用 LayerNorm (因为只在最后一个时间步运行)
+        # Decoder: 使用LayerNorm/GroupNorm + ECA
         self.decoder = RecordDecoder(
             config=config['decoder_config'],
             n_class=n_class,
-            norm_decoder=norm  # 添加 norm_decoder 参数
+            norm_decoder=norm
         )
 
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
         """
-        Forward pass RECORD-OI model for SINGLE TIMESTEP
-        *** 修改为支持 Hybrid Norm 的版本 ***
+        Forward pass RECORD-OI model (single timestep)
+        *** 修改版: 支持Hybrid Norm架构 ***
 
-        @param x: input tensor with shape (B, C, H, W) - SINGLE timestep
+        @param x: input tensor with shape (B, C, H, W) - 单个时间步
         @return: ConfMap prediction with shape (B, n_classes, H, W)
         """
-        # x shape: (B, C, H, W) - 单个时间步
+        B, C, H, W = x.shape
+        assert len(x.shape) == 4, "Online模式输入应该是4D: (B, C, H, W)"
 
-        # 1. Stem part (BN) - 直接处理
+        # 1. Stem forward (BatchNorm part)
         stem_features = self.encoder.forward_stem(x)
 
-        # 2. Recurrent part (GN/LayerNorm) - 单步处理
-        # 获取当前的隐藏状态
+        # 2. Recurrent forward (GroupNorm/LayerNorm part)
+        # 获取当前hidden states
         h_list = self.encoder.h_list
         c_list = self.encoder.c_list
 
-        # 执行单步 recurrent forward
+        # 单步recurrent forward
         (st_features_backbone,
          st_features_lstm2,
          st_features_lstm1), new_h_list, new_c_list = self.encoder.forward_recurrent_step(
             stem_features, h_list, c_list
         )
 
-        # 更新隐藏状态
+        # 更新encoder的hidden states
         self.encoder.h_list = new_h_list
         self.encoder.c_list = new_c_list
 
-        # 3. Decoder (使用最后一个时间步的特征)
+        # 3. Decoder forward (使用ECA注意力机制)
         confmap_pred = self.decoder(st_features_backbone, st_features_lstm2, st_features_lstm1)
 
         return self.sigmoid(confmap_pred)
