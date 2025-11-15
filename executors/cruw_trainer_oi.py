10e40eac221ab738f66c8db2a89f2188045bb39f
lyqFlaringFire <2513598550@qq.com>
2025-11-15 15:45:56 +0800

    online第一次修改

--- executors/cruw_trainer_oi.py
+++ executors/cruw_trainer_oi.py
@@ -6,31 +6,49 @@ from datasets.cruw.collate_functions import cr_collate
 from evaluation.postprocess import post_process_single_frame_cruw, write_dets_results_single_frame
 from cruw.eval.rod.rod_eval_utils import accumulate, summarize
 
+
 class CruwExecutorOI(CruwExecutor):
 
     def training_step(self, batch, batch_id):
         """
         Perform one training step (forward + backward) on a batch of data.
+        *** 修改为支持 Hybrid Norm 的版本 ***
+
         @param batch: data batch from the dataloader
         @param batch_id: id of the current batch
         @return: loss value to log
         """
         # Get data
-        ra_maps = batch['radar_data']  # N, H, W, C
-        confmap_gts = batch['anno']['confmaps']
+        ra_maps = batch['radar_data']  # (B, C, T, H, W)
+        confmap_gts = batch['anno']['confmaps']  # (B, n_class, T, H, W)
         image_paths = batch['image_paths']
+
+        B, C, T, H, W = ra_maps.shape
+
+        # 初始化隐藏状态
+        self.model.encoder.__init_hidden__()
+
         total_loss = 0
-        for t in range(ra_maps.shape[2]):
-            if t == 0:
-                self.model.encoder.__init_hidden__()
-            confmap_pred = self.model(ra_maps[:, :, t])
+
+        # 逐时间步处理 (符合 Online 推理逻辑)
+        for t in range(T):
+            # 获取当前时间步的输入: (B, C, H, W)
+            x_t = ra_maps[:, :, t]
+
+            # Forward pass (会自动更新隐藏状态)
+            confmap_pred = self.model(x_t)
+
+            # 计算损失
             loss = self.loss_fct(confmap_pred, confmap_gts[:, :, t])
             total_loss += loss
 
-        #TODO: loss depending on the frames
-        total_loss = total_loss / ra_maps.shape[2]
-        self.log('train_loss', total_loss, on_step=True, on_epoch=True, logger=True)
-        self.log('hp/train_loss', total_loss, on_epoch=True)
+        # 平均损失
+        total_loss = total_loss / T
+
+        self.log('train_loss', total_loss, on_step=True, on_epoch=True, logger=True,
+                 sync_dist=True, batch_size=self.batch_size)
+        self.log('hp/train_loss', total_loss, on_epoch=True, sync_dist=True,
+                 batch_size=self.batch_size)
 
         return total_loss
 
@@ -45,26 +63,40 @@ class CruwExecutorOI(CruwExecutor):
     def validation_step(self, batch, batch_id):
         """
         Perform a validation step (forward pass) on a batch of data.
+        *** 修改为支持 Hybrid Norm 的版本 ***
+
         @param batch: data batch from the dataloader
         @param batch_id: id of the current batch
         """
         # Get data
-        ra_maps = batch['radar_data']  # N, H, W, C
+        ra_maps = batch['radar_data']  # (B, C, T, H, W)
         confmap_gts = batch['anno']['confmaps']
         image_paths = batch['image_paths']
         obj_infos = batch['anno']['obj_infos']
 
-        assert ra_maps.shape[2] == 1 and ra_maps.shape[0] == 1, "Batch size and window size must be one for inference."
-        confmap_pred = self.forward(ra_maps[:, :, 0])
+        # Online inference: 每次处理单个时间步
+        assert ra_maps.shape[2] == 1 and ra_maps.shape[0] == 1, \
+            "Batch size and window size must be one for online inference."
 
+        # 提取单个时间步: (1, C, 1, H, W) -> (1, C, H, W)
+        x_t = ra_maps[:, :, 0]
+
+        # Forward (会自动使用当前隐藏状态)
+        confmap_pred = self.forward(x_t)
+
+        # 计算损失
         loss = self.loss_fct(confmap_pred, confmap_gts[:, :, 0])
 
-        self.log('val_loss', loss, on_step=False, on_epoch=True, prog_bar=False, logger=True)
-        self.log('hp/val_loss', loss, on_epoch=True)
+        self.log('val_loss', loss, on_step=False, on_epoch=True, prog_bar=False,
+                 logger=True, sync_dist=True, batch_size=self.batch_size)
+        self.log('hp/val_loss', loss, on_epoch=True, sync_dist=True,
+                 batch_size=self.batch_size)
 
     def test_step(self, batch, batch_id):
         """
         Perform a test step (forward pass + evaluation) on a batch of data.
+        *** 修改为支持 Hybrid Norm 的版本 ***
+
         @param batch: data batch from the dataloader
         @param batch_id: id of the current batch
         """
@@ -93,31 +125,51 @@ class CruwExecutorOI(CruwExecutor):
             frame_name = image_paths[0][-1][0].split('/')[-1].split('.')[0].split('_')[0]
             frame_id = int(frame_name)
 
-        assert ra_maps.shape[2] == 1 and ra_maps.shape[0] == 1, "Batch size and window size must be one for inference."
-        confmap_pred = self.forward(ra_maps[:, :, 0])
+        # Online inference: 每次处理单个时间步
+        assert ra_maps.shape[2] == 1 and ra_maps.shape[0] == 1, \
+            "Batch size and window size must be one for online inference."
+
+        # 提取单个时间步: (1, C, 1, H, W) -> (1, C, H, W)
+        x_t = ra_maps[:, :, 0]
+
+        # Forward (会自动使用当前隐藏状态)
+        confmap_pred = self.forward(x_t)
 
         # Write results
-        res_final = post_process_single_frame_cruw(confmap_pred[0].cpu(), self.cruw_dataset_obj, self.config)
-        write_dets_results_single_frame(res_final, frame_id, save_path, self.cruw_dataset_obj)
+        res_final = post_process_single_frame_cruw(confmap_pred[0].cpu(),
+                                                   self.cruw_dataset_obj, self.config)
+        write_dets_results_single_frame(res_final, frame_id, save_path,
+                                        self.cruw_dataset_obj)
 
     def evaluate_rodnet_(self):
-        ols_thrs = np.around(np.linspace(0.5, 0.9, int(np.round((0.9 - 0.5) / 0.05) + 1), endpoint=True), decimals=2)
-        rec_thrs = np.around(np.linspace(0.0, 1.0, int(np.round((1.0 - 0.0) / 0.01) + 1), endpoint=True), decimals=2)
-        out_eval = accumulate(self.evalImgs_all, self.n_frames_all, ols_thrs, rec_thrs, self.cruw_dataset_obj, log=False)
+        """
+        Evaluate RODNET performance
+        """
+        ols_thrs = np.around(np.linspace(0.5, 0.9, int(np.round((0.9 - 0.5) / 0.05) + 1),
+                                         endpoint=True), decimals=2)
+        rec_thrs = np.around(np.linspace(0.0, 1.0, int(np.round((1.0 - 0.0) / 0.01) + 1),
+                                         endpoint=True), decimals=2)
+        out_eval = accumulate(self.evalImgs_all, self.n_frames_all, ols_thrs, rec_thrs,
+                              self.cruw_dataset_obj, log=False)
         stats = summarize(out_eval, ols_thrs, rec_thrs, self.cruw_dataset_obj, gl=False)
-        self.logger.log_metrics({"AP/Overall": stats[0] * 100,
-                   "AR/Overall": stats[1] * 100})
 
+        self.logger.log_metrics({"AP/Overall": stats[0] * 100,
+                                 "AR/Overall": stats[1] * 100})
         self.logger.log_metrics({"hp/AP": stats[0] * 100,
                                  "hp/AR": stats[1] * 100})
 
     def on_validation_start(self):
+        """在验证开始时初始化隐藏状态"""
         self.model.encoder.__init_hidden__()
 
     def on_test_start(self):
+        """在测试开始时初始化隐藏状态"""
         self.model.encoder.__init_hidden__()
+
     def on_validation_end(self):
+        """在验证结束时重置隐藏状态"""
         self.model.encoder.__init_hidden__()
 
     def on_test_end(self):
+        """在测试结束时重置隐藏状态"""
         self.model.encoder.__init_hidden__()
--- models/record_oi.py
+++ models/record_oi.py
@@ -5,34 +5,70 @@ from .layers.bottleneck_lstm import BottleneckLSTM
 from utils.models_utils import _make_divisible
 
 """
-Entrainement en initialisant les états sur les k premières frames puis prédire les suivantes
-
-Online learning LSTM biblio 
-
+Online Inference version of RECORD model with Hybrid Norm + ECA
 """
+
+
 class RecordOI(nn.Module):
     def __init__(self, config, in_channels=8, norm='layer', n_class=3):
         """
         RECurrent Online object detectOR (RECORD) model class for online inference
+        *** 修改版 (Hybrid Norm + ECA) ***
+
         @param config: configuration file of the model
-        @param alpha: expansion factor to modify the size of the model (default: 1.0)
         @param in_channels: number of input channels (default: 8)
-        @param norm: type of normalisation (default: LayerNorm). Other normalisation are not supported yet.
+        @param norm: type of normalisation for recurrent parts (default: 'layer')
         @param n_class: number of classes (default: 3)
-        @param shallow: load a shallow version of RECORD (fewer channels in the decoder)
         """
         super(RecordOI, self).__init__()
-        self.encoder = RecordEncoder(config=config['encoder_config'], in_channels=in_channels, norm=norm)
-        self.decoder = RecordDecoder(config=config['decoder_config'], n_class=n_class)
+
+        # Hybrid Norm: Stem 用 BN，Recurrent 用 LayerNorm/GN(1)
+        self.encoder = RecordEncoder(
+            config=config['encoder_config'],
+            in_channels=in_channels,
+            norm_stem='bn',  # Stem 部分使用 BatchNorm
+            norm_recurrent=norm  # Recurrent 部分使用 LayerNorm
+        )
+
+        # Decoder 使用 LayerNorm (因为只在最后一个时间步运行)
+        self.decoder = RecordDecoder(
+            config=config['decoder_config'],
+            n_class=n_class,
+            norm_decoder=norm  # 添加 norm_decoder 参数
+        )
+
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
         """
-        Forward pass RECORD-OI model
-        @param x: input tensor with shape (B, C, T, H, W) where T is the number of timesteps
-        @return: ConfMap prediction of the last time step with shape (B, n_classes, H, W)
-        """
-        st_features_lstm1, st_features_lstm2, st_features_backbone = self.encoder(x)
-        confmap_pred = self.decoder(st_features_lstm1, st_features_lstm2, st_features_backbone)
-        return self.sigmoid(confmap_pred)
+        Forward pass RECORD-OI model for SINGLE TIMESTEP
+        *** 修改为支持 Hybrid Norm 的版本 ***
 
+        @param x: input tensor with shape (B, C, H, W) - SINGLE timestep
+        @return: ConfMap prediction with shape (B, n_classes, H, W)
+        """
+        # x shape: (B, C, H, W) - 单个时间步
+
+        # 1. Stem part (BN) - 直接处理
+        stem_features = self.encoder.forward_stem(x)
+
+        # 2. Recurrent part (GN/LayerNorm) - 单步处理
+        # 获取当前的隐藏状态
+        h_list = self.encoder.h_list
+        c_list = self.encoder.c_list
+
+        # 执行单步 recurrent forward
+        (st_features_backbone,
+         st_features_lstm2,
+         st_features_lstm1), new_h_list, new_c_list = self.encoder.forward_recurrent_step(
+            stem_features, h_list, c_list
+        )
+
+        # 更新隐藏状态
+        self.encoder.h_list = new_h_list
+        self.encoder.c_list = new_c_list
+
+        # 3. Decoder (使用最后一个时间步的特征)
+        confmap_pred = self.decoder(st_features_backbone, st_features_lstm2, st_features_lstm1)
+
+        return self.sigmoid(confmap_pred)
