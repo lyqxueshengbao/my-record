c8b1c4b85d7d516652d8bd91756c11181ca55941
lyqFlaringFire <2513598550@qq.com>
2025-11-16 18:54:48 +0800

    第四次修改online

--- executors/cruw_trainer_oi.py
+++ executors/cruw_trainer_oi.py
@@ -6,6 +6,7 @@ from datasets.cruw.collate_functions import cr_collate
 from evaluation.postprocess import post_process_single_frame_cruw, write_dets_results_single_frame
 from cruw.eval.rod.rod_eval_utils import accumulate, summarize
 
+
 class CruwExecutorOI(CruwExecutor):
 
     def training_step(self, batch, batch_id):
@@ -16,18 +17,29 @@ class CruwExecutorOI(CruwExecutor):
         @return: loss value to log
         """
         # Get data
-        ra_maps = batch['radar_data']  # N, H, W, C
-        confmap_gts = batch['anno']['confmaps']
+        ra_maps = batch['radar_data']  # N, C, T, H, W (T=32)
+        confmap_gts = batch['anno']['confmaps']  # N, Class, T, H, W
         image_paths = batch['image_paths']
         total_loss = 0
-        for t in range(ra_maps.shape[2]):
-            if t == 0:
-                self.model.encoder.__init_hidden__()
+
+        # <--- MODIFICATION START ---
+        # 关键修复：
+        # 我们在 *批次(batch)* 开始时重置隐藏状态，而不是在 *循环(loop)* 内部。
+        # 这确保了模型在 32 帧的窗口内保持记忆连续性，
+        # 从而正确学习时序依赖关系。
+        self.model.encoder.__init_hidden__()
+        # <--- MODIFICATION END ---
+
+        for t in range(ra_maps.shape[2]):  # 循环 32 帧
+            # <--- MODIFICATION START ---
+            # 删除了原有的 'if t == 0: self.model.encoder.__init_hidden__()'
+            # <--- MODIFICATION END ---
+
             confmap_pred = self.model(ra_maps[:, :, t])
             loss = self.loss_fct(confmap_pred, confmap_gts[:, :, t])
             total_loss += loss
 
-        #TODO: loss depending on the frames
+        # TODO: loss depending on the frames (如我们之前讨论的，这个TODO是作者想做加权损失的标记)
         total_loss = total_loss / ra_maps.shape[2]
         self.log('train_loss', total_loss, on_step=True, on_epoch=True, logger=True)
         self.log('hp/train_loss', total_loss, on_epoch=True)
@@ -48,18 +60,16 @@ class CruwExecutorOI(CruwExecutor):
         @param batch: data batch from the dataloader
         @param batch_id: id of the current batch
         """
-        # --- 在开头添加 ---
-        if self.val_frame_counter % self.win_size == 0:
-            self.model.encoder.__init_hidden__()
-        self.val_frame_counter += 1
-        # --- 添加结束 ---
+        # (此函数无需修改)
         # Get data
-        ra_maps = batch['radar_data']  # N, H, W, C
-        confmap_gts = batch['anno']['confmaps']
+        ra_maps = batch['radar_data']  # N, C, T, H, W (T=1)
+        confmap_gts = batch['anno']['confmaps']  # N, Class, T, H, W
         image_paths = batch['image_paths']
         obj_infos = batch['anno']['obj_infos']
 
         assert ra_maps.shape[2] == 1 and ra_maps.shape[0] == 1, "Batch size and window size must be one for inference."
+
+        # 'forward' 会调用 self.model()，它会连续使用并更新隐藏状态
         confmap_pred = self.forward(ra_maps[:, :, 0])
 
         loss = self.loss_fct(confmap_pred, confmap_gts[:, :, 0])
@@ -73,11 +83,7 @@ class CruwExecutorOI(CruwExecutor):
         @param batch: data batch from the dataloader
         @param batch_id: id of the current batch
         """
-        # --- 在开头添加 ---
-        if self.test_frame_counter % self.win_size == 0:
-            self.model.encoder.__init_hidden__()
-        self.test_frame_counter += 1
-        # --- 添加结束 ---
+        # (此函数无需修改)
         ra_maps = batch['radar_data']
         image_paths = batch['image_paths']
         confmap_gts = batch['anno']
@@ -104,6 +110,8 @@ class CruwExecutorOI(CruwExecutor):
             frame_id = int(frame_name)
 
         assert ra_maps.shape[2] == 1 and ra_maps.shape[0] == 1, "Batch size and window size must be one for inference."
+
+        # 'forward' 会调用 self.model()，它会连续使用并更新隐藏状态
         confmap_pred = self.forward(ra_maps[:, :, 0])
 
         # Write results
@@ -113,23 +121,32 @@ class CruwExecutorOI(CruwExecutor):
     def evaluate_rodnet_(self):
         ols_thrs = np.around(np.linspace(0.5, 0.9, int(np.round((0.9 - 0.5) / 0.05) + 1), endpoint=True), decimals=2)
         rec_thrs = np.around(np.linspace(0.0, 1.0, int(np.round((1.0 - 0.0) / 0.01) + 1), endpoint=True), decimals=2)
-        out_eval = accumulate(self.evalImgs_all, self.n_frames_all, ols_thrs, rec_thrs, self.cruw_dataset_obj, log=False)
+        out_eval = accumulate(self.evalImgs_all, self.n_frames_all, ols_thrs, rec_thrs, self.cruw_dataset_obj,
+                              log=False)
         stats = summarize(out_eval, ols_thrs, rec_thrs, self.cruw_dataset_obj, gl=False)
         self.logger.log_metrics({"AP/Overall": stats[0] * 100,
-                   "AR/Overall": stats[1] * 100})
+                                 "AR/Overall": stats[1] * 100})
 
         self.logger.log_metrics({"hp/AP": stats[0] * 100,
                                  "hp/AR": stats[1] * 100})
 
     def on_validation_start(self):
+        # (此函数无需修改)
+        # 在验证开始时（即处理第一个序列之前）重置状态。
         self.model.encoder.__init_hidden__()
-        self.val_frame_counter = 0  # 添加此行
 
     def on_test_start(self):
+        # (此函数无需修改)
+        # 在测试开始时（即处理第一个序列之前）重置状态。
+        # 评估脚本 'evaluation/eval_cruw.py'
+        # 会为 *每个* 序列调用一次 'trainer.test()'，
+        # 因此这个钩子会在每个序列开始时被正确调用。
         self.model.encoder.__init_hidden__()
-        self.test_frame_counter = 0  # 添加此行
+
     def on_validation_end(self):
+        # (此函数无需修改)
         self.model.encoder.__init_hidden__()
 
     def on_test_end(self):
+        # (此函数无需修改)
         self.model.encoder.__init_hidden__()
